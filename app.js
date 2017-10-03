3756e22950d8d1942a8493d225262a252212e076
thewisenerd <thewisenerd@protonmail.com>
2017-10-03 21:05:28 +0530

    Revert "rename to `writr`; nuke views"
    This reverts commit c353b267e8ab6cf17dca72793c1b654195087fc6.

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var api = require('./routes/api');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -17,7 +22,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', api);
+app.use('/', index);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
